b5d73a15585143dcf48be96edcaff98d486b56aa
Andrey <sevostyanov.a@rusrobots.ru>
2023-06-14 16:30:12 +0300

    basket development started

---
+++ components/v-product-card-second.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="product-card-second">
+        <div class="product-card-second__wrap-info">
+            <nuxt-link to="/" class="product-card-second__block-image">
+                <img src="@/assets/images/products/product-4.jpg" class="product-card-second__image" alt="">
+            </nuxt-link>
+            <div class="product-card-second__block-info">
+                <span class="product-card-second__name">Graystone vase</span>
+                <span class="product-card-second__desc">A timeless ceramic vase with a tri color grey glaze.</span>
+            </div>
+        </div>
+        <v-counter @quantity="getQuantity" />
+        <span class="product-card-second__price">£85</span>
+    </div>
+</template>
+
+<script setup>
+
+    const getQuantity = (value) => {
+        // console.log(value)
+    }
+</script>
--- pages/cart.vue
+++ pages/cart.vue
@@ -1,6 +1,20 @@
 <template>
    <main class="page cart-page">
-
+        <div class="container">
+            <div class="cart-page__wrap-content">
+                <v-title type-title="h1" modifiers="title--h1">Your shopping cart</v-title>
+                <div class="cart-page__table">
+                    <ul class="cart-page__list-names list-reset">
+                        <li class="cart-page__li-names">Product</li>
+                        <li class="cart-page__li-names">Quantity</li>
+                        <li class="cart-page__li-names">Total</li>
+                    </ul>
+                    <div class="cart-page__table-products">
+                        <v-product-card-second />
+                    </div>
+                </div>
+            </div>
+        </div>
     </main>
 </template>
 
